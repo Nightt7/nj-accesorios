5c8cc4b779c7f007c966180458ffc9e617b25c14
Night <101990128+Nightt7@users.noreply.github.com>
2022-05-18 16:15:12 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,10 @@ games {'common'}
 
 fx_version 'adamant'
 
+author 'Nightt#8899 & !Javichu#7020'
+
+description 'FiveM ESX Script police clothing accessories'
+
 shared_scripts {
     '@es_extended/imports.lua',
     'config.lua'
